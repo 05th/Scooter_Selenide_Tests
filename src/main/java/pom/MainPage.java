e7c4086bc3e346d9e4b1750aa35ba05764dc6935
Russ <qatsby@yandex.com>
2023-10-16 22:43:47 -0400

    fix errors

--- src/main/java/pom/MainPage.java
+++ src/main/java/pom/MainPage.java
@@ -1,8 +1,13 @@
 package pom;
 
+import org.openqa.selenium.By;
+
 public class MainPage {
 
+
     private String baseUrl = "https://qa-scooter.praktikum-services.ru/";
+    @FindBy(how = How.xpath, using = "topOrderButton";
+
 
 
 }
