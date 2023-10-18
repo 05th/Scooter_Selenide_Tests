a47555a63976ce3f300d98382950deae62eee15f
Russ <qatsby@yandex.com>
2023-10-17 20:54:28 -0400

    selectors added

--- src/main/java/pom/MainPage.java
+++ src/main/java/pom/MainPage.java
@@ -1,13 +1,24 @@
 package pom;
+import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.Sleeper;
 
-import org.openqa.selenium.By;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selectors.*;
+
 
 public class MainPage {
 
-
+    private WebDriver driver;
     private String baseUrl = "https://qa-scooter.praktikum-services.ru/";
-    @FindBy(how = How.xpath, using = "topOrderButton";
+    private SelenideElement topOrderButton = $(byXpath(".//button[@class='Button_Button__ra12g']"));
+    private SelenideElement lowerOrderButton = $(byXpath(".//button[contains(@class, 'Button_Middle__1CSJM')]"));
+    private SelenideElement headerOfQuestions = $(byXpath(".//div[text()='Вопросы о важном']"));
+    private SelenideElement cookieButton = $(byXpath("//*[@id='rcc-confirm-button']"));
 
+    public MainPage (WebDriver driver) {
+        this.driver = driver;
+    }
 
 
 }
